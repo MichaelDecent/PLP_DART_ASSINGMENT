0dfdea8dbf2d95d13b3c971b1084714728e6cb9e
MichaelDecent <111002205+MichaelDecent@users.noreply.github.com>
2023-02-21 18:03:17 +0100

    dart assignment

---
+++ Dart_Functions_Assignment.dart
@@ -0,0 +1,32 @@
+// A function that adds two numbers
+num addTwo(num n1, num n2) {
+  return (n1 + n2);
+}
+
+//A function that subtracts two numbers
+num subtractTwo(num n1, num n2) {
+  return (n1 - n2);
+}
+
+//A function that multiplies two numbers
+num multiplyTwo(num n1, num n2) {
+  return (n1 * n2);
+}
+
+// A function that divides two numbers
+num divideTwo(num n1, num n2) {
+  return (n1 / n2);
+}
+
+// A function that returns the length of that string.
+int stringLength(String word) {
+  int len = word.length;
+  return (len);
+}
+
+// A function that returns the first element of that list.
+dynamic getFirstElement(List<dynamic> myList) {
+  dynamic firstElement = myList.elementAt(0);
+  return (firstElement);
+}
+
